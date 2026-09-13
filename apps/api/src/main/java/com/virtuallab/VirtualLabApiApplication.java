afbb19e47fc18fd59fbad9da603f98e93ee289dd
hyeonszz <shj270515@gmail.com>
2026-09-13 14:13:39 +0900

    chore: API와 로컬 개발 환경 초기 구성

---
+++ apps/api/src/main/java/com/virtuallab/VirtualLabApiApplication.java
@@ -0,0 +1,13 @@
+package com.virtuallab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualLabApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualLabApiApplication.class, args);
+	}
+
+}
---
+++ apps/api/src/test/java/com/virtuallab/VirtualLabApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtuallab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualLabApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
